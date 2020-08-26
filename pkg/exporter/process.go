9fbba7a2f83bbc7e17cefc372dae541d45c33d12
srikartati <stati@vmware.com>
2020-08-26 11:36:08 -0700

    Change klog version to v2 (#16)

--- pkg/exporter/process.go
+++ pkg/exporter/process.go
@@ -20,7 +20,7 @@ import (
 	"net"
 	"time"
 
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 
 	"github.com/vmware/go-ipfix/pkg/entities"
 )
--- pkg/registry/build_registry/build_registry.go
+++ pkg/registry/build_registry/build_registry.go
@@ -28,7 +28,7 @@ import (
 	"strconv"
 	"strings"
 
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 
 	"github.com/vmware/go-ipfix/pkg/entities"
 )
