28665c2d0f3a24f0f60a77fdd325dfe666c5bcd6
zyiou <zyiou@vmware.com>
2020-09-09 11:04:20 -0700

    Add encoding function

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -20,6 +20,18 @@ import (
 	"io"
 )
 
+// Encode writes the multiple inputs of different possible datatypes to the io writer.
+func Encode(buff io.Writer, byteOrder binary.ByteOrder, inputs ...interface{}) error {
+	var err error
+	for _, in := range inputs {
+		err = binary.Write(buff, byteOrder, in)
+		if err != nil {
+			return fmt.Errorf("Error in encoding data %v: %v", in, err)
+		}
+	}
+	return nil
+}
+
 // Decode decodes data from io reader to specified interfaces
 /* Example:
 	var num1 uint16
@@ -28,10 +40,10 @@ import (
 	// decode and output corresponding uint16 and uint32 number into num1 and num2 respectively
 	err := Decode(buffer, &num1, &num2)
 */
-func Decode(buffer io.Reader, outputs ...interface{}) error {
+func Decode(buffer io.Reader, byteOrder binary.ByteOrder, outputs ...interface{}) error {
 	var err error
 	for _, out := range outputs {
-		err = binary.Read(buffer, binary.BigEndian, out)
+		err = binary.Read(buffer, byteOrder, out)
 		if err != nil {
 			return fmt.Errorf("Error in decoding data: %v", err)
 		}
