a9574349958cd146cf89b3c4808fa774fcd1384e
srikartati <stati@vmware.com>
2020-11-06 13:06:34 +0530

    Add standalone ipfix collector (#71)
    This is for Antrea e2e tests.
    Binary can be generated through "collector" target that is
    defined as part of Makefile.

---
+++ cmd/collector/collector.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"k8s.io/component-base/logs"
+	"k8s.io/klog"
+
+	"github.com/vmware/go-ipfix/pkg/collector"
+	"github.com/vmware/go-ipfix/pkg/entities"
+	"github.com/vmware/go-ipfix/pkg/registry"
+)
+
+const (
+	logToStdErrFlag = "logtostderr"
+)
+
+var (
+	IPFIXAddr      string
+	IPFIXPort      uint16
+	IPFIXTransport string
+)
+
+func initLoggingToFile(fs *pflag.FlagSet) {
+	var err error
+	var logToStdErr bool
+
+	logToStdErr, err = fs.GetBool(logToStdErrFlag)
+	if err != nil {
+		// Should not happen. Return for safety.
+		return
+	}
+	if logToStdErr {
+		// Logging to files is not enabled.
+		return
+	}
+}
+
+func addIPFIXFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&IPFIXAddr, "ipfix.addr", "", "IPFIX collector address")
+	fs.Uint16Var(&IPFIXPort, "ipfix.port", 4739, "IPFIX collector port")
+	fs.StringVar(&IPFIXTransport, "ipfix.transport", "tcp", "IPFIX collector transport layer")
+}
+
+func printIPFIXMessage(msg *entities.Message) {
+	var buf bytes.Buffer
+	fmt.Fprint(&buf, "\nIPFIX-HDR:\n")
+	fmt.Fprintf(&buf, "  version: %v,  Message Length: %v\n", msg.Version, msg.BufferLength)
+	fmt.Fprintf(&buf, "  Exported Time: %v (%v)\n", msg.ExportTime, time.Unix(int64(msg.ExportTime), 0))
+	fmt.Fprintf(&buf, "  Sequence No.: %v,  Observation Domain ID: %v\n", msg.SeqNumber, msg.ObsDomainID)
+
+	if msg.Set.GetSetType() == entities.Template {
+		fmt.Fprint(&buf, "TEMPLATE SET:\n")
+		for i, record := range msg.Set.GetRecords() {
+			fmt.Fprintf(&buf, "  TEMPLATE RECORD-%d:\n", i)
+			for _, ie := range record.GetInfoElements() {
+				fmt.Fprintf(&buf, "    %s: len=%d (enterprise ID = %d) \n", ie.Element.Name, ie.Element.Len, ie.Element.EnterpriseId)
+			}
+		}
+	} else {
+		fmt.Fprint(&buf, "DATA SET:\n")
+		for i, record := range msg.Set.GetRecords() {
+			fmt.Fprintf(&buf, "  DATA RECORD-%d:\n", i)
+			for _, ie := range record.GetInfoElements() {
+				fmt.Fprintf(&buf, "    %s: %v \n", ie.Element.Name, ie.Value)
+			}
+		}
+	}
+	klog.Infof(buf.String())
+}
+
+func signalHandler(stopCh chan struct{}, messageReceived chan *entities.Message) {
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	for {
+		select {
+		case msg := <-messageReceived:
+			printIPFIXMessage(msg)
+		case <-signalCh:
+			close(stopCh)
+			return
+		}
+	}
+}
+
+func run() error {
+	klog.Info("Starting IPFIX collector")
+
+	// Load the IPFIX global registry
+	registry.LoadRegistry()
+
+	var netAddr net.Addr
+	var err error
+	if IPFIXTransport == "tcp" {
+		netAddr, err = net.ResolveTCPAddr("tcp", IPFIXAddr+":"+strconv.Itoa(int(IPFIXPort)))
+		if err != nil {
+			return err
+		}
+	} else if IPFIXTransport == "udp" {
+		netAddr, err = net.ResolveUDPAddr("udp", IPFIXAddr+":"+strconv.Itoa(int(IPFIXPort)))
+		if err != nil {
+			return err
+		}
+	} else {
+		return fmt.Errorf("input given ipfix.transport flag is not supported or valid")
+	}
+	// Initialize collecting process
+	cp, err := collector.InitCollectingProcess(netAddr, 65535, 0)
+	if err != nil {
+		return err
+	}
+	// Start listening to connections and receiving messages.
+	messageReceived := make(chan *entities.Message)
+	go func() {
+		go cp.Start()
+		msgCount := 0
+		// This can be simplified if collector uses a message channel.
+		// Are there any use cases for having list of messages in collector process.
+		for {
+			ipfixMessages := cp.GetMessages()
+			currentLen := len(ipfixMessages)
+			if currentLen > msgCount {
+				for i := msgCount; i < currentLen; i++ {
+					klog.Info("Processing IPFIX message")
+					messageReceived <- ipfixMessages[i]
+					msgCount++
+				}
+			}
+			time.Sleep(5 * time.Millisecond)
+		}
+	}()
+
+	stopCh := make(chan struct{})
+	go signalHandler(stopCh, messageReceived)
+
+	<-stopCh
+	// Stop the collector process
+	cp.Stop()
+	klog.Info("Stopping IPFIX collector")
+	return nil
+}
+
+func newCollectorCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:  "ipfix-collector",
+		Long: "IPFIX collector to decode the exported flow records",
+		Run: func(cmd *cobra.Command, args []string) {
+			initLoggingToFile(cmd.Flags())
+			if err := run(); err != nil {
+				klog.Fatalf("Error when running IPFIX collector: %v", err)
+			}
+		},
+	}
+	flags := cmd.Flags()
+	addIPFIXFlags(flags)
+	// Install command line flags
+	flags.AddGoFlagSet(flag.CommandLine)
+	return cmd
+}
+
+func main() {
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	command := newCollectorCommand()
+	if err := command.Execute(); err != nil {
+		logs.FlushLogs()
+		os.Exit(1)
+	}
+}
