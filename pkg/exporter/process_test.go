0e2a6c2640ca82191aa8b89e5bff0c2d66a1f952
Srikar Tati <stati@vmware.com>
2021-04-29 11:05:05 -0700

    Add Kafka TLS support to the producer.
    Also, enhance the input consumption when initializing the
    Kafka producer.
    
    Unit tests added for TLS cert based kafka connection.

--- pkg/exporter/process_test.go
+++ pkg/exporter/process_test.go
@@ -25,94 +25,7 @@ import (
 
 	"github.com/vmware/go-ipfix/pkg/entities"
 	"github.com/vmware/go-ipfix/pkg/registry"
-)
-
-const (
-	// TODO: update the certs before 2022-02-01
-	fakeCACert = `-----BEGIN CERTIFICATE-----
-MIIDHTCCAgWgAwIBAgIUe4gJCUY6fAkjtBFUjZTWmG3X4h0wDQYJKoZIhvcNAQEL
-BQAwHjELMAkGA1UEBhMCVVMxDzANBgNVBAoMBnZtd2FyZTAeFw0yMTAyMDIwMTIw
-MzlaFw0zMTAxMzEwMTIwMzlaMB4xCzAJBgNVBAYTAlVTMQ8wDQYDVQQKDAZ2bXdh
-cmUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC2ww7EiAT71r2J5Nj+
-XcfsfG3JIx7NYXPoEwiyasc5O0ntmcsOhWX295PkM/nq9gxUKRCRafjczl+KaCYG
-GgUXUoNJnJN2Oy+uNEOwwJN/jH2ElMltAGyot6BE8QJwKxPqkzW9eY1XSAdelU86
-QaAUlit8mXUx/8eOiafxcEQtZdnXyZV5ceniC5+QHQBpmcmssohS4rEFVFacIdSB
-BxSzSI0UbT9fRL0ZDP03ki1kYbVTrLUSuYzVdGCbU7PKSkotP6umzIrcALKAS2wY
-WhE0lig0S2PFJEHrPJUHWtdRSDttY4n+Lrhsa7t+SNYWQyPWnG/FSfviw32vyHki
-K3NnAgMBAAGjUzBRMB0GA1UdDgQWBBR/fNirU4kPi113EE7J0M7k+JCmwDAfBgNV
-HSMEGDAWgBR/fNirU4kPi113EE7J0M7k+JCmwDAPBgNVHRMBAf8EBTADAQH/MA0G
-CSqGSIb3DQEBCwUAA4IBAQCXAY9rwUiGxDhObL4x83CZO+aLkl0yxwd2Qp+FaH/o
-wkkLf/RlYIHpwAlaFly/z/MsThuQs0Ke2MZ11M/kDpjERCLTcoUldniezP6sT0Ha
-vMzhhBivc96seWxc74sNc0+kGtygyvtV72AeKLtsM3qwZB3k11xmdGEf/DNZRC9s
-iSeMFT+VQn7FS61YP2SPD0MQG5pcc7tkFbEbriC+0GuEc+emgQXZfb6lTxcCLthD
-1ZefCNIvrZfwddLuO9UAS6vEwV1ngvnaxAYTiIVDX4JLxQryDSEY7SfdrEuY3HKi
-q2Cb+bFmy+CfJsqSYvf+lxalkEiZnR4abVzUKb7JRYII
------END CERTIFICATE-----`
-	fakeKey = `-----BEGIN RSA PRIVATE KEY-----
-MIIEpAIBAAKCAQEAoHOXzKUXHoPvhC82ut0XRE1obaKw9i8gwejf+GxzJDNEMlpI
-WhcpNvFaydpF/jPP27eYLP1M6ow7ZKeXJ/khe+wLvfWsYBXBBz2qL++KjcEFhjop
-mI1vQnNEss8lHSZ8whMTTybBM/UGsX4+6RZwp5BUecMZm+maXq5FSmMBB9dEBXpI
-DlvEyjwPugyprkuXgWPtdLqzDMKMKUehm4fNcHvUVFiSWNMoTB7AVmSzXBHaLkC8
-j1XJ0rnxE+uawoPp8QxqpOK8p46TKMuXtvzOgIwJ77a+lGkNXycHiQAMZD1tkAkD
-tiFcxHLxeZ6AuoD3iXiXNn1iAD8FwTr80IFUVwIDAQABAoIBAG6sbPeepFzLGqot
-uu2X1RWq4y5EexXEmi7Gx75eZ+l/ZznoJmHl+erI/hufcIoQCP8AG3pk2eorAkjC
-hLPw1xTYU50a2WSJfNTWxw47h3GRw5K7ibeNT0IhqjBAS9RYmNNxPu9oxvlkuNIz
-R4eVj+000+ANIBv4PR+zy12s/qf9z2uDiAK+wPC3koxfJEPzwN0MbHNoqCrSS4WA
-U5Ia5gAErpjk/TB1CIn/VXbWfajGtPLQFIaj1vaWCfYincoHihqzzr+0eZI3GmEq
-MzhgSgarWpVC8sUtmwwYmDwd5LBI1sB4J2jD+MZdkLgQPv88LMrpvHefVoIGCBnj
-htnTqhECgYEAzoz+bxKsc1YVbx0nfkvsXmHTiu8ymXQkASNafCaGTUokH7ehVmBg
-qla2GA67B/yU0CuPpXYOcy2drvHNY+4cW6JPMmR+kxOiUMUGlNDr2alB5dTSvPc3
-VQlrAACff+L2dZnee0KZL/lWFAnUQvr/yUFDJkauu3r5S1red+AQ1VkCgYEAxt1J
-fQkf60/ByIJ5FIKw7PM/qn6pZUwy7lrbSNZs46DKr3eBhPWkit1Y2Vr1CWPenZ8m
-xeLNpvfDX37TQJVjstVIiOzNguXY1OGLJMiKqk8s0mCj0hazCRNE0cqoQBYT3VrJ
-2Qa1NG5WjHqvnO45YNjotygzjf9Tbp3oN8cRUS8CgYEAmh0DXtbdCzWIyp5DMG3v
-0EBHudtdsrfAgYTiZ83K2XJfX7cAz1Ub6xzFw0+CZ6QjWOuXw4pBn9jCVZ0fdddd
-G1YQp3XqJ004B/HqRjFYONCcVaMlFk+S3oUnbBoK6D3tTLPhF7EKYYw1mL/4u31S
-StTCCJYmm8ZWqc4EPgdWKAkCgYAmvvzPiNSYQ3ztNUakEWapdCeGb5FPtE4EFN2+
-zlB+9VgoYe9xWW8Kw1/JUaiGgNfh/B1Q1wTcsE93rr04SyMWt+mfDAxqoZ/ismMq
-8ovZ7P09rCifCV8uuHtjKMopWmPacBL93wm1Qn5Idqr6t4uka/7DmOialRHLbqbN
-H1MnbwKBgQCjRVBouJWDCKlkkoTHqMdcMnFERwW7ccklH2eM7ErxWnxS3EUrN8dT
-xqsurz+i8+xaAi7TMiIG4D3PprDBoomQFOs8skvwrOEqbz7rWAiaf8FoMFd1f5pH
-1LjjKO6VscEZKi3pNhUQLDszjjyM7ol3k4sN79z2FKZ/cFf0SnoN0Q==
------END RSA PRIVATE KEY-----`
-	fakeCert = `-----BEGIN CERTIFICATE-----
-MIIC3TCCAcWgAwIBAgIUc9wOjUVSKpTIZmPlqzvSJf+l0kMwDQYJKoZIhvcNAQEL
-BQAwHjELMAkGA1UEBhMCVVMxDzANBgNVBAoMBnZtd2FyZTAeFw0yMTAyMDIwMTM0
-MDBaFw0zMTAxMzEwMTM0MDBaMB4xCzAJBgNVBAYTAlVTMQ8wDQYDVQQKDAZ2bXdh
-cmUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCgc5fMpRceg++ELza6
-3RdETWhtorD2LyDB6N/4bHMkM0QyWkhaFyk28VrJ2kX+M8/bt5gs/UzqjDtkp5cn
-+SF77Au99axgFcEHPaov74qNwQWGOimYjW9Cc0SyzyUdJnzCExNPJsEz9Qaxfj7p
-FnCnkFR5wxmb6ZperkVKYwEH10QFekgOW8TKPA+6DKmuS5eBY+10urMMwowpR6Gb
-h81we9RUWJJY0yhMHsBWZLNcEdouQLyPVcnSufET65rCg+nxDGqk4rynjpMoy5e2
-/M6AjAnvtr6UaQ1fJweJAAxkPW2QCQO2IVzEcvF5noC6gPeJeJc2fWIAPwXBOvzQ
-gVRXAgMBAAGjEzARMA8GA1UdEQQIMAaHBH8AAAEwDQYJKoZIhvcNAQELBQADggEB
-AKtIqKgLhYT21Ngm4eiKuWma0KLEJGJMFSijHO7LLwq3I90A6OA6sjmv9OsfNsRi
-sBZSm8IT4+SIG3o4tAhgFfZjdmBNuY1ImDFoUfV6wfddi3cihNb5kLu2qv1SlUI4
-P1EAXOLPAEvs7uJbrWNCi6sZD6AThDh7vQzqHuFkTRBxN7fI7uLznl5ue3JW9Z77
-Cg8bBqbQ25otfo06J5QLXxXdHKipHZVlGyC+cbrqyKgOL/vYJZLr0maZQHwBzUOG
-i5kKdHpUBCRoun1ZKzumSZZ3xXeDMdm49uCtFrr5ylfWuEOsentOfGaElaUSdAud
-63BrM6BAmp/kxVLXZSo6JfI=
------END CERTIFICATE-----`
-	fakeKey2 = `-----BEGIN PRIVATE KEY-----
-MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQg1h0K9jGfyBQMttaz
-ija4rnsXfTQf1KvXl2o9SABhtvmhRANCAAQnICXGTyc72J2mpIgbZz3mvgmqUzGJ
-FaU0IQHwImuqwIjbsJtnj6XgozycBwTPGPkuQeyKp3k3ADE7UOCqsSOH
------END PRIVATE KEY-----
-`
-	fakeCert2 = `-----BEGIN CERTIFICATE-----
-MIIB+jCCAaCgAwIBAgIJALfqenQRnGoHMAoGCCqGSM49BAMCMHgxCzAJBgNVBAYT
-AlhYMQwwCgYDVQQIDANOL0ExDDAKBgNVBAcMA04vQTEgMB4GA1UECgwXU2VsZi1z
-aWduZWQgY2VydGlmaWNhdGUxKzApBgNVBAMMIjEyMC4wLjAuMTogU2VsZi1zaWdu
-ZWQgY2VydGlmaWNhdGUwHhcNMjAxMTA4MDgwNjQ2WhcNMjIxMTA4MDgwNjQ2WjB4
-MQswCQYDVQQGEwJYWDEMMAoGA1UECAwDTi9BMQwwCgYDVQQHDANOL0ExIDAeBgNV
-BAoMF1NlbGYtc2lnbmVkIGNlcnRpZmljYXRlMSswKQYDVQQDDCIxMjAuMC4wLjE6
-IFNlbGYtc2lnbmVkIGNlcnRpZmljYXRlMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcD
-QgAEJyAlxk8nO9idpqSIG2c95r4JqlMxiRWlNCEB8CJrqsCI27CbZ4+l4KM8nAcE
-zxj5LkHsiqd5NwAxO1DgqrEjh6MTMBEwDwYDVR0RBAgwBocEfwAAATAKBggqhkjO
-PQQDAgNIADBFAiEAzUT2hG3WChJh8cBo7EMQan2eJiF96OlSB+rWKKMaoGACIGOp
-RVaPKj9ad0Z/3GiwaxtW+74bvc2vF3JS9cRU6DhY
------END CERTIFICATE-----
-`
+	"github.com/vmware/go-ipfix/pkg/test"
 )
 
 func init() {
@@ -489,7 +402,7 @@ func TestExportingProcessWithTLS(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Got error when resolving tcp address: %v", err)
 	}
-	cer, err := tls.X509KeyPair([]byte(fakeCert), []byte(fakeKey))
+	cer, err := tls.X509KeyPair([]byte(test.FakeCert), []byte(test.FakeKey))
 	if err != nil {
 		t.Error(err)
 		return
@@ -527,7 +440,7 @@ func TestExportingProcessWithTLS(t *testing.T) {
 		ObservationDomainID: 1,
 		TempRefTimeout:      0,
 		IsEncrypted:         true,
-		CACert:              []byte(fakeCACert),
+		CACert:              []byte(test.FakeCACert),
 	}
 	exporter, err := InitExportingProcess(input)
 	if err != nil {
@@ -571,7 +484,7 @@ func TestExportingProcessWithDTLS(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Got error when resolving udp address: %v", err)
 	}
-	cert, err := tls.X509KeyPair([]byte(fakeCert2), []byte(fakeKey2))
+	cert, err := tls.X509KeyPair([]byte(test.FakeCert2), []byte(test.FakeKey2))
 	if err != nil {
 		t.Error(err)
 		return
@@ -612,7 +525,7 @@ func TestExportingProcessWithDTLS(t *testing.T) {
 		ObservationDomainID: 1,
 		TempRefTimeout:      0,
 		IsEncrypted:         true,
-		CACert:              []byte(fakeCert2),
+		CACert:              []byte(test.FakeCert2),
 	}
 	exporter, err := InitExportingProcess(input)
 	if err != nil {
