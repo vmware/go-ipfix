c2d7045af345f78d6d42c018b739663353c36e0d
Srikar Tati <stati@vmware.com>
2021-06-03 17:19:01 -0700

    Modify the proto format of the message
    Instead of using oneof proto property, we move to
    protoreflect.Message to support multiple proto schemas.
    Advantage is that users consuming our Kafka producer can define
    their own proto directly and implement the converter interface.

--- pkg/producer/convertor/test/flowtype2.go
+++ pkg/producer/convertor/test/flowtype2.go
@@ -17,6 +17,7 @@ package test
 import (
 	"net"
 
+	"google.golang.org/protobuf/reflect/protoreflect"
 	"k8s.io/klog/v2"
 
 	"github.com/vmware/go-ipfix/pkg/entities"
@@ -33,96 +34,107 @@ func RegisterFlowType2() convertor.IPFIXToKafkaConvertor {
 	return &convertRecordToFlowType2{}
 }
 
-func (c *convertRecordToFlowType2) ConvertIPFIXMsgToFlowMsgs(msg *entities.Message) []*protobuf.FlowMessage {
-	convertRecordToFlowMsg := func(msg *entities.Message, record entities.Record) *protobuf.FlowMessage {
+func (c *convertRecordToFlowType2) ConvertIPFIXMsgToFlowMsgs(msg *entities.Message) []protoreflect.Message {
+	convertRecordToFlowMsg := func(msg *entities.Message, record entities.Record) protoreflect.Message {
 		flowType2 := &protobuf.FlowType2{}
 		flowType2.TimeReceived = msg.GetExportTime()
 		flowType2.SequenceNumber = msg.GetSequenceNum()
 		flowType2.ObsDomainID = msg.GetObsDomainID()
 		flowType2.ExportAddress = msg.GetExportAddress()
-		for _, ie := range record.GetOrderedElementList() {
-			switch ie.Element.Name {
-			case "flowStartSeconds":
-				flowType2.TimeFlowStartInSecs = ie.Value.(uint32)
-			case "flowEndSeconds":
-				flowType2.TimeFlowEndInSecs = ie.Value.(uint32)
-			case "sourceIPv4Address", "sourceIPv6Address":
-				if flowType2.SrcIP != "" {
-					klog.Warningf("Do not expect source IP: %v to be filled already", flowType2.SrcIP)
-				}
-				flowType2.SrcIP = ie.Value.(net.IP).String()
-			case "destinationIPv4Address", "destinationIPv6Address":
-				if flowType2.DstIP != "" {
-					klog.Warningf("Do not expect destination IP: %v to be filled already", flowType2.DstIP)
-				}
-				flowType2.DstIP = ie.Value.(net.IP).String()
-			case "sourceTransportPort":
-				flowType2.SrcPort = uint32(ie.Value.(uint16))
-			case "destinationTransportPort":
-				flowType2.DstPort = uint32(ie.Value.(uint16))
-			case "protocolIdentifier":
-				flowType2.Proto = uint32(ie.Value.(uint8))
-			case "packetTotalCount":
-				flowType2.PacketsTotal = ie.Value.(uint64)
-			case "octetTotalCount":
-				flowType2.BytesTotal = ie.Value.(uint64)
-			case "packetDeltaCount":
-				flowType2.PacketsDelta = ie.Value.(uint64)
-			case "octetDeltaCount":
-				flowType2.BytesDelta = ie.Value.(uint64)
-			case "reversePacketTotalCount":
-				flowType2.ReversePacketsTotal = ie.Value.(uint64)
-			case "reverseOctetTotalCount":
-				flowType2.ReverseBytesTotal = ie.Value.(uint64)
-			case "reversePacketDeltaCount":
-				flowType2.ReversePacketsDelta = ie.Value.(uint64)
-			case "reverseOctetDeltaCount":
-				flowType2.ReverseBytesDelta = ie.Value.(uint64)
-			case "sourcePodNamespace":
-				flowType2.SrcPodNamespace = ie.Value.(string)
-			case "sourcePodName":
-				flowType2.SrcPodName = ie.Value.(string)
-			case "sourceNodeName":
-				flowType2.SrcNodeName = ie.Value.(string)
-			case "destinationPodNamespace":
-				flowType2.DstPodNamespace = ie.Value.(string)
-			case "destinationPodName":
-				flowType2.DstPodName = ie.Value.(string)
-			case "destinationNodeName":
-				flowType2.DstNodeName = ie.Value.(string)
-			case "destinationClusterIPv4", "destinationClusterIPv6":
-				if flowType2.DstClusterIP != "" {
-					klog.Warningf("Do not expect destination cluster IP: %v to be filled already", flowType2.DstClusterIP)
-				}
-				flowType2.DstClusterIP = ie.Value.(net.IP).String()
-			case "destinationServicePort":
-				flowType2.DstServicePort = uint32(ie.Value.(uint16))
-			case "destinationServicePortName":
-				flowType2.DstServicePortName = ie.Value.(string)
-			case "ingressNetworkPolicyName":
-				flowType2.IngressPolicyName = ie.Value.(string)
-			case "ingressNetworkPolicyNamespace":
-				flowType2.IngressPolicyNamespace = ie.Value.(string)
-			case "egressNetworkPolicyName":
-				flowType2.EgressPolicyName = ie.Value.(string)
-			case "egressNetworkPolicyNamespace":
-				flowType2.EgressPolicyNamespace = ie.Value.(string)
-			default:
-				klog.Warningf("There is no field with name: %v in flow message (.proto schema)", ie.Element.Name)
-			}
-		}
-		return &protobuf.FlowMessage{
-			FlowType: &protobuf.FlowMessage_Flow2{Flow2: flowType2},
-		}
+		addAllFieldsToFlowType2(flowType2, record)
+		return flowType2.ProtoReflect()
 	}
 	set := msg.GetSet()
 	if set.GetSetType() == entities.Template {
 		return nil
 	}
 	records := set.GetRecords()
-	flowMsgs := make([]*protobuf.FlowMessage, len(records))
+	flowMsgs := make([]protoreflect.Message, len(records))
 	for i, record := range records {
 		flowMsgs[i] = convertRecordToFlowMsg(msg, record)
 	}
 	return flowMsgs
+}
+
+func (c *convertRecordToFlowType2) ConvertIPFIXRecordToFlowMsg(record entities.Record) protoreflect.Message {
+	// We add the fields from the record directly and do not add the fields from
+	// the IPIFX message to flowType1 kafka flow message.
+	flowType2 := &protobuf.FlowType2{}
+	addAllFieldsToFlowType2(flowType2, record)
+
+	return flowType2.ProtoReflect()
+}
+
+func addAllFieldsToFlowType2(flowMsg *protobuf.FlowType2, record entities.Record) {
+	for _, ie := range record.GetOrderedElementList() {
+		switch ie.Element.Name {
+		case "flowStartSeconds":
+			flowMsg.TimeFlowStartInSecs = ie.Value.(uint32)
+		case "flowEndSeconds":
+			flowMsg.TimeFlowEndInSecs = ie.Value.(uint32)
+		case "sourceIPv4Address", "sourceIPv6Address":
+			if flowMsg.SrcIP != "" {
+				klog.Warningf("Do not expect source IP: %v to be filled already", flowMsg.SrcIP)
+			}
+			flowMsg.SrcIP = ie.Value.(net.IP).String()
+		case "destinationIPv4Address", "destinationIPv6Address":
+			if flowMsg.DstIP != "" {
+				klog.Warningf("Do not expect destination IP: %v to be filled already", flowMsg.DstIP)
+			}
+			flowMsg.DstIP = ie.Value.(net.IP).String()
+		case "sourceTransportPort":
+			flowMsg.SrcPort = uint32(ie.Value.(uint16))
+		case "destinationTransportPort":
+			flowMsg.DstPort = uint32(ie.Value.(uint16))
+		case "protocolIdentifier":
+			flowMsg.Proto = uint32(ie.Value.(uint8))
+		case "packetTotalCount":
+			flowMsg.PacketsTotal = ie.Value.(uint64)
+		case "octetTotalCount":
+			flowMsg.BytesTotal = ie.Value.(uint64)
+		case "packetDeltaCount":
+			flowMsg.PacketsDelta = ie.Value.(uint64)
+		case "octetDeltaCount":
+			flowMsg.BytesDelta = ie.Value.(uint64)
+		case "reversePacketTotalCount":
+			flowMsg.ReversePacketsTotal = ie.Value.(uint64)
+		case "reverseOctetTotalCount":
+			flowMsg.ReverseBytesTotal = ie.Value.(uint64)
+		case "reversePacketDeltaCount":
+			flowMsg.ReversePacketsDelta = ie.Value.(uint64)
+		case "reverseOctetDeltaCount":
+			flowMsg.ReverseBytesDelta = ie.Value.(uint64)
+		case "sourcePodNamespace":
+			flowMsg.SrcPodNamespace = ie.Value.(string)
+		case "sourcePodName":
+			flowMsg.SrcPodName = ie.Value.(string)
+		case "sourceNodeName":
+			flowMsg.SrcNodeName = ie.Value.(string)
+		case "destinationPodNamespace":
+			flowMsg.DstPodNamespace = ie.Value.(string)
+		case "destinationPodName":
+			flowMsg.DstPodName = ie.Value.(string)
+		case "destinationNodeName":
+			flowMsg.DstNodeName = ie.Value.(string)
+		case "destinationClusterIPv4", "destinationClusterIPv6":
+			if flowMsg.DstClusterIP != "" {
+				klog.Warningf("Do not expect destination cluster IP: %v to be filled already", flowMsg.DstClusterIP)
+			}
+			flowMsg.DstClusterIP = ie.Value.(net.IP).String()
+		case "destinationServicePort":
+			flowMsg.DstServicePort = uint32(ie.Value.(uint16))
+		case "destinationServicePortName":
+			flowMsg.DstServicePortName = ie.Value.(string)
+		case "ingressNetworkPolicyName":
+			flowMsg.IngressPolicyName = ie.Value.(string)
+		case "ingressNetworkPolicyNamespace":
+			flowMsg.IngressPolicyNamespace = ie.Value.(string)
+		case "egressNetworkPolicyName":
+			flowMsg.EgressPolicyName = ie.Value.(string)
+		case "egressNetworkPolicyNamespace":
+			flowMsg.EgressPolicyNamespace = ie.Value.(string)
+		default:
+			klog.Warningf("There is no field with name: %v in flow message (.proto schema)", ie.Element.Name)
+		}
+	}
 }
