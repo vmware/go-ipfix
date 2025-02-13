1eae9921f750e1aa200c80fac53815959a535006
Antonin Bas <antonin.bas@broadcom.com>
2025-02-13 14:46:56 -0800

    Add buffered exporter for IPFIX messages (#409)
    SendDataRecords was added in #391 as a way to export data records more
    efficiently. However, this method is not convenient to use for clients
    which need to mutate the records after sending them (as is the case when
    aggregation is used). To work around this issue, we introduce a buffered
    exporter. The buffered exporter wraps the standard exporter
    process. Whenever a data record is added, it is serialized to a byte
    buffer immediately, and the buffer is sent when it reaches the maximum
    message size.
    
    Note that for convenience the buffered exporter also supports JSON
    records, but in this case it just acts as a passthrough to the regular
    ("unbuffered") exporter process.
    
    Signed-off-by: Antonin Bas <antonin.bas@broadcom.com>

---
+++ pkg/exporter/buffered_test.go
@@ -0,0 +1,168 @@
+// Copyright 2025 VMware, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package exporter
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/vmware/go-ipfix/pkg/entities"
+	"github.com/vmware/go-ipfix/pkg/registry"
+)
+
+func TestBufferedExporter(t *testing.T) {
+	// Create local server for testing
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	require.NoError(t, err)
+	conn, err := net.ListenUDP("udp", udpAddr)
+	require.NoError(t, err)
+	t.Log("Created local server on random available port for testing")
+
+	receivedLengthsCh := make(chan int, 10)
+	go func() {
+		defer conn.Close()
+		b := make([]byte, 512)
+		for {
+			n, err := conn.Read(b)
+			if err != nil {
+				return
+			}
+			receivedLengthsCh <- n
+		}
+	}()
+
+	// Create exporter using local server info
+	input := ExporterInput{
+		CollectorAddress:    conn.LocalAddr().String(),
+		CollectorProtocol:   conn.LocalAddr().Network(),
+		ObservationDomainID: 1,
+		MaxMsgSize:          512,
+	}
+	exporter, err := InitExportingProcess(input)
+	require.NoError(t, err)
+	t.Logf("Created exporter connecting to local server with address: %s", conn.LocalAddr().String())
+	defer exporter.CloseConnToCollector()
+
+	bufferedExporter := NewBufferedIPFIXExporter(exporter)
+
+	// Create template record with two fields
+	templateID := exporter.NewTemplateID()
+	elements := make([]entities.InfoElementWithValue, 0)
+	ieSrc, err := registry.GetInfoElement("sourceIPv4Address", registry.IANAEnterpriseID)
+	require.NoError(t, err, "Did not find the element with name sourceIPv4Address")
+	elements = append(elements, entities.NewIPAddressInfoElement(ieSrc, nil))
+	ieDst, err := registry.GetInfoElement("destinationIPv4Address", registry.IANAEnterpriseID)
+	require.NoError(t, err, "Did not find the element with name destinationIPv4Address")
+	elements = append(elements, entities.NewIPAddressInfoElement(ieDst, nil))
+	template := entities.NewTemplateRecordFromElements(templateID, elements, false)
+
+	require.NoError(t, bufferedExporter.AddRecord(template))
+	select {
+	case <-receivedLengthsCh:
+		break
+	case <-time.After(100 * time.Millisecond):
+		require.Fail(t, "Expected template not received")
+	}
+
+	record := func() entities.Record {
+		elements := []entities.InfoElementWithValue{
+			entities.NewIPAddressInfoElement(ieSrc, net.ParseIP("1.2.3.4")),
+			entities.NewIPAddressInfoElement(ieDst, net.ParseIP("5.6.7.8")),
+		}
+		return entities.NewDataRecordFromElements(templateID, elements, false)
+	}()
+	// Each record will be 8B. The message size has been set to 512B above.
+	// The overheade per message is 16 (message header) + 4 (set header).
+	// So we can fit 61 records per message.
+	// If we send 200 records, we will need 4 messages.
+	for range 200 {
+		require.Equal(t, 8, record.GetRecordLength()) // sanity check
+		require.NoError(t, bufferedExporter.AddRecord(record))
+	}
+	require.NoError(t, bufferedExporter.Flush())
+
+	timerCh := time.After(100 * time.Millisecond)
+	for _, expectedBytesReceived := range []int{508, 508, 508, 156} {
+		select {
+		case bytesReceived := <-receivedLengthsCh:
+			assert.Equal(t, expectedBytesReceived, bytesReceived)
+		case <-timerCh:
+			require.Fail(t, "Expected message not received")
+		}
+	}
+}
+
+func BenchmarkBufferedExporter(b *testing.B) {
+	// Create local server for testing
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	require.NoError(b, err)
+	conn, err := net.ListenUDP("udp", udpAddr)
+	require.NoError(b, err)
+
+	go func() {
+		defer conn.Close()
+		b := make([]byte, 512)
+		for {
+			if _, err := conn.Read(b); err != nil {
+				return
+			}
+		}
+	}()
+
+	// Create exporter using local server info
+	input := ExporterInput{
+		CollectorAddress:    conn.LocalAddr().String(),
+		CollectorProtocol:   conn.LocalAddr().Network(),
+		ObservationDomainID: 1,
+		MaxMsgSize:          512,
+	}
+	exporter, err := InitExportingProcess(input)
+	require.NoError(b, err)
+	b.Logf("Created exporter connecting to local server with address: %s", conn.LocalAddr().String())
+	defer exporter.CloseConnToCollector()
+
+	bufferedExporter := NewBufferedIPFIXExporter(exporter)
+
+	// Create template record with two fields
+	templateID := exporter.NewTemplateID()
+	elements := make([]entities.InfoElementWithValue, 0)
+	ieSrc, err := registry.GetInfoElement("sourceIPv4Address", registry.IANAEnterpriseID)
+	require.NoError(b, err, "Did not find the element with name sourceIPv4Address")
+	elements = append(elements, entities.NewIPAddressInfoElement(ieSrc, nil))
+	ieDst, err := registry.GetInfoElement("destinationIPv4Address", registry.IANAEnterpriseID)
+	require.NoError(b, err, "Did not find the element with name destinationIPv4Address")
+	elements = append(elements, entities.NewIPAddressInfoElement(ieDst, nil))
+	template := entities.NewTemplateRecordFromElements(templateID, elements, false)
+
+	require.NoError(b, bufferedExporter.AddRecord(template))
+
+	record := func() entities.Record {
+		elements := []entities.InfoElementWithValue{
+			entities.NewIPAddressInfoElement(ieSrc, net.ParseIP("1.2.3.4")),
+			entities.NewIPAddressInfoElement(ieDst, net.ParseIP("5.6.7.8")),
+		}
+		return entities.NewDataRecordFromElements(templateID, elements, false)
+	}()
+
+	b.ResetTimer()
+
+	for range b.N {
+		require.NoError(b, bufferedExporter.AddRecord(record))
+	}
+}
